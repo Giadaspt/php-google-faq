f583812961c2dbe7444669da8b55a2625ab2bb10
Giadaspt <saputo.giada@gmail.com>
2021-12-21 15:23:07 +0100

    primo commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*Riscrivere questa pagina del sito google: https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+Consegna minima:
+Stampare tutti i paragrafi suddividendo titolo e contenuto
+Una volta risolto questo requisito base potete migliorare il lavoro con:
+- creare uno spazio fra un sottoparagrafo e l’altro
+- gestire il paragrafo “Perché il mio account è associato a un paese?” con gli elenchi
+- gestire i link interni al testo
+Preoccupatevi della fedeltà grafica solo una volta risolti i problemi PHP
+*/
+
+
+
+
+
+?>
