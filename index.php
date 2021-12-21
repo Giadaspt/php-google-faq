e3afa0e1ec19c121b19d2888b95d308700f71325
Giadaspt <saputo.giada@gmail.com>
2021-12-21 17:05:56 +0100

    in progress

--- index.php
+++ index.php
@@ -55,3 +55,45 @@ Preoccupatevi della fedeltà grafica solo una volta risolti i problemi PHP
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    ul li {
+      list-style: none;
+    }
+  </style>
+</head>
+<body>
+  <ul>
+      <li>
+        
+        <h3>
+          <?php
+            foreach ($faq as $question){ ?>
+              <?php 
+                echo "$question[question] <br>";
+              ?>
+            <?php } ?>
+        </h3>
+        <p>
+        <?php
+            foreach ($faq as $answer){ ?>
+              <?php 
+                echo " $answer[answer] <br>";
+              ?>
+            <?php } ?>
+        </p>
+      </li>
+    <li>
+      
+    </li>
+  </ul>
+  
+</body>
+</html>
